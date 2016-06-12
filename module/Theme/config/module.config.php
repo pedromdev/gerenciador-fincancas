27757e53096caa0be2420b5ca284838474eaaa17
Pedro Alves <pedromarcelodesaalves@gmail.com>
2016-06-12 02:33:58 -0300

    View Helper para o tema

--- module/Theme/config/module.config.php
+++ module/Theme/config/module.config.php
@@ -1,3 +1,11 @@
 <?php
 
-return array();
+namespace Theme;
+
+return array(
+    'view_helpers' => array(
+        'factories' => array(
+            'theme' => View\ThemeFactory::class,
+        ),
+    ),
+);
